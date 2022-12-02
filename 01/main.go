677259dc1d31efcf1c42d1ed2f362ebce65d114a
Kyle <kyle@waryway.com>
2022-12-02 01:15:14 -0800

    Create main.go

---
+++ 01/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// A for Rock, B for Paper, and C for Scissors.
+// X for Rock, Y for Paper, and Z for Scissors.
+// (1 for Rock, 2 for Paper, and 3 for Scissors)
+// 0 if you lost, 3 if the round was a draw, and 6 if you won
+func main() {
+	input := `1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`
+
+	elves := strings.Split(input, "\n\n")
+	var cals []int
+	for _, value := range elves {
+		sub := strings.Split(value, "\n")
+		total := 0
+		for i := 0; i < len(sub); i++ {
+			intVal, _ := strconv.Atoi(sub[i])
+			total += intVal
+		}
+
+		cals = append(cals, total)
+	}
+	fmt.Println(maxArr(cals))
+}
+
+func maxArr(input []int) int {
+	max := 0
+	for _, val := range input {
+		if val > max {
+			max = val
+		}
+	}
+	return max
+}
