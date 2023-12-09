9c82741bae48bd42536e7657058543031ab46c5a
Kyle <kyle@waryway.com>
2023-12-08 23:44:39 -0600

    Create main.go

---
+++ 2023/09/01/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type numbList struct {
+	Numbers []int
+	History *numbList
+}
+
+func (n *numbList) populateList(newList []int) {
+	n.Numbers = newList
+	allZeroes := true
+	var history []int
+	for i:=1; i < len(n.Numbers); i++ {
+		diff := n.Numbers[i] - n.Numbers[i-1]
+		if diff != 0 {
+			allZeroes = false
+		}
+		history = append(history, diff)
+	}
+	if allZeroes {
+		history = append(history, 0)
+		return
+	} else {
+		n.History = &numbList{}
+		n.History.populateList(history)
+		n.Numbers = append(n.Numbers, n.History.Numbers[len(n.History.Numbers)-1] + n.Numbers[len(n.Numbers)-1])
+	}
+
+}
+
+var input = `0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45`
+
+
+func main() {
+	fullList := strings.Split(input, "\n")
+	total := 0
+	for _, v := range fullList {
+		var numberList []int
+		numberStrings := strings.Split(v, " ")
+		for _, z := range numberStrings {
+			value, _ := strconv.Atoi(z)
+			numberList = append(numberList, value)
+		}
+		var numbs numbList
+		numbs.populateList(numberList)
+		fmt.Println("adding", numbs.Numbers[len(numbs.Numbers)-1])
+		total += numbs.Numbers[len(numbs.Numbers)-1]
+	}
+	fmt.Println(total)
+
+	var numbs numbList
+	numbs.populateList([]int{0,   3,   6,  9,  12,  15})
+	fmt.Println(numbs.Numbers[len(numbs.Numbers)-1])
+}
